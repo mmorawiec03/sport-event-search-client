55b959880a4e03baba53d9eaba2fca469073e9fa
Mateusz Morawiec <mateuszmorawiec98@wp.pl>
2020-07-29 20:14:52 +0200

    Delete tokens and user data from storage on logout.

--- shared/Header.js
+++ shared/Header.js
@@ -3,17 +3,27 @@ import { Text, View, ImageBackground, Alert } from 'react-native';
 import { headerStyles } from '../styles/HeaderStyles';
 import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import { AuthContext } from '../contexts/AuthContext';
+import { deleteTokens } from '../storage/Token';
+import { deleteUserData } from '../storage/UserData';
 
 
 export default function Header({ navigation, title }) {
     const { dispatch } = useContext(AuthContext);
 
     const logout = () => {
+        deleteTokens().then(() => {
+            deleteUserData().then(() => {
+                dispatch({type: 'LOGOUT'})
+            });
+        });
+    }
+
+    const logoutAlert = () => {
         Alert.alert(
             'Logout',
             'Are you sure you want to logout?',
             [
-                {text: 'Logout', onPress: () => dispatch({type: 'LOGOUT'})},
+                {text: 'Logout', onPress: logout},
                 {text: 'Cancel', style: 'cancel'}
             ],
             {cancelable: true}
@@ -28,7 +38,7 @@ export default function Header({ navigation, title }) {
         <View style={headerStyles.header}>
             <Feather name='menu' size={28} color='white' onPress={showMenu} />
             <Text style={headerStyles.headerTitle}>{title}</Text>
-            <MaterialCommunityIcons name='logout' size={32} color='white' onPress={logout} />
+            <MaterialCommunityIcons name='logout' size={32} color='white' onPress={logoutAlert} />
         </View>
     );
 }
